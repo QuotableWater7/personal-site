6c0509041b855a6bcb6b9b758d5a893e9f721440
JT <jt@buildingconnected.com>
2020-05-19 22:00:27 -0700

    Fix bug

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 const PORT = 8000
 
-app.use(express.static('src/static'))
+app.use(express.static('src/server/static'))
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`)
